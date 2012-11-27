d9fcd9e579133b9bba749ea2cc4979ce87db537f
JWhy <jwhy@jwhy.de>
2012-11-27 12:04:49 +0100

    Changed Classloader for BehaviorLoader and added example class
    Signed-off-by: JWhy <jwhydev@jwhy.de>

---
+++ src/main/java/org/jwhy/craftbutler/Behavior.java
@@ -0,0 +1,5 @@
+package org.jwhy.craftbutler;
+
+public abstract class Behavior {
+
+}
--- src/main/java/org/jwhy/craftbutler/BehaviorLoader.java
+++ src/main/java/org/jwhy/craftbutler/BehaviorLoader.java
@@ -1,69 +1,41 @@
 package org.jwhy.craftbutler;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+
+import org.bukkit.plugin.Plugin;
 
 public class BehaviorLoader {
 	
 	private int loaded;
 	private File behavior_directory;
+	private Plugin plugin;
 
 	/**
 	 * @param dir Directory containing the behavior classes
 	 */
-	public BehaviorLoader(File dir){
+	public BehaviorLoader(File dir, Plugin plugin){
 		dir.mkdirs();
 		this.behavior_directory = dir;
+		this.plugin = plugin;
 	}
 	
 	public void loadBehaviors(){
-		File[] class_list = CraftButlerUtils.getDirectoryFiles(behavior_directory, ".class");
-		CraftButlerUtils.logDebug(class_list.toString());
-		URL[] urls = new URL[class_list.length];
-		for(int i = 0; i < class_list.length; i++){
-			try {
-				urls[i] = class_list[i].toURL();
-			} catch (Exception e) {
+		URL[] url_list = CraftButlerUtils.getDirectoryFilesAsURL(behavior_directory, ".jar");
+		CraftButlerUtils.logDebug(url_list.toString(), this.plugin);
+		
+		for(URL url : url_list){
+			String classname = CraftButlerUtils.removeExtension(url.getFile());
+			try{
+		        URLClassLoader loader = new URLClassLoader(new URL[]{url});
+				Class cl = Class.forName(classname, true, loader);
+			}catch(ClassNotFoundException e){
+				CraftButlerUtils.logDebug("Class from " + url.getFile() + " could not be loaded!", this.plugin);
+			}catch (Exception e) {
+				CraftButlerUtils.logDebug(url.getFile() + " could not be loaded!", this.plugin);
 				e.printStackTrace();
 			}
 		}
-		ClassLoader cl = new URLClassLoader(urls);
-		cl.close();
-	}
-	
-	public int getNumLoadedBehaviour(){
-		return(this.loaded);
-	}
-	
-	@SuppressWarnings("rawtypes")
-	public void close() {
-		try {
-		   Class clazz = java.net.URLClassLoader.class;
-		   java.lang.reflect.Field ucp = clazz.getDeclaredField("ucp");
-		   ucp.setAccessible(true);
-		   Object sun_misc_URLClassPath = ucp.get(this);
-		   java.lang.reflect.Field loaders = 
-		      sun_misc_URLClassPath.getClass().getDeclaredField("loaders");
-		   loaders.setAccessible(true);
-		   Object java_util_Collection = loaders.get(sun_misc_URLClassPath);
-		   for (Object sun_misc_URLClassPath_JarLoader :
-		        ((java.util.Collection) java_util_Collection).toArray()) {
-		      try {
-		         java.lang.reflect.Field loader = 
-		            sun_misc_URLClassPath_JarLoader.getClass().getDeclaredField("jar");
-		         loader.setAccessible(true);
-		         Object java_util_jar_JarFile = 
-		            loader.get(sun_misc_URLClassPath_JarLoader);
-		         ((java.util.jar.JarFile) java_util_jar_JarFile).close();
-		      } catch (Throwable t) {
-		         // if we got this far, this is probably not a JAR loader so skip it
-		      }
-		   }
-		} catch (Throwable t) {
-		   // probably not a SUN VM
-		}
-		return;
 	}
 }
