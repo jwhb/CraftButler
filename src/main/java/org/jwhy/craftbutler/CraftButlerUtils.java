7668e245239f53c21b0084a88e58a3daca35c6de
JWhy <jwhy@jwhy.de>
2012-11-27 12:11:30 +0100

    Added debug log function and unified function parameters
    Signed-off-by: JWhy <jwhydev@jwhy.de>

--- src/main/java/org/jwhy/craftbutler/CraftButlerUtils.java
+++ src/main/java/org/jwhy/craftbutler/CraftButlerUtils.java
@@ -5,8 +5,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
@@ -61,8 +64,21 @@ public class CraftButlerUtils {
     }
 
 
-	public static void logDebug(String string) {
-		
+	public static void logDebug(String msg, Plugin plugin) {
+		if(CraftButler.debug){
+			String prefix = "Debug: ";
+			if(plugin == null){
+				//Anonymous logging
+				Bukkit.getConsoleSender().sendMessage(prefix + msg);
+			}else{
+				plugin.getLogger().log(Level.INFO, prefix + msg);
+			}
+		}
+	}
+	
+	public static String removeExtension(String filename) {
+		int ext = filename.lastIndexOf(".");
+		return(filename.substring(0, ext));
 	}
 	
 	public static String getExtension(String filename) {
@@ -74,19 +90,32 @@ public class CraftButlerUtils {
 		return(getExtension(file.getName()));
 	}
 	
-	public static File[] getDirectoryFiles(File dir, String extension){
+	public static File[] getDirectoryFiles(File dir, String ext){
 		File[] contents = dir.listFiles();
 		for(int i = 0; i<contents.length;i++){
 		  //cf => current file
 		  File cf = contents[i];
-		  logDebug(getExtension(cf));
-		  if(cf instanceof File && getExtension(cf) == extension){
+		  logDebug(getExtension(cf), null);
+		  if(cf instanceof File && getExtension(cf) == ext){
 			  //Filter everything but files
 			  contents[i] = cf;
 		  }
 		}
 		return(contents);		
 	}
-	
+
+
+	public static URL[] getDirectoryFilesAsURL(File behavior_directory, String ext) {
+		File[] file_list = CraftButlerUtils.getDirectoryFiles(behavior_directory, ext);
+		URL[] url_list = new URL[file_list.length];
+		int i = 0;
+		for(File file : file_list){
+		  try {
+			url_list[i] = file.toURI().toURL();
+		  } catch (MalformedURLException e){e.printStackTrace();}
+		}
+		
+		return(url_list);
+	}
 	
 }
