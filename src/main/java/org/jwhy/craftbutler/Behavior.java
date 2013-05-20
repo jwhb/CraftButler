0c23a48c2a4c60321f4f868d359d8fff4f0188d4
Jasper Wiegratz <jwhy@jwhy.de>
2013-05-20 14:51:46 +0200

    Implemented Behavior class template
    Signed-off-by: JWhy <jwhydev@jwhy.de>

--- src/main/java/org/jwhy/craftbutler/Behavior.java
+++ src/main/java/org/jwhy/craftbutler/Behavior.java
@@ -2,4 +2,16 @@ package org.jwhy.craftbutler;
 
 public abstract class Behavior {
 
+	public String name;
+	@SuppressWarnings("unused")
+	private CraftButler butler;
+	
+	public Behavior(CraftButler butler){
+		this.butler = butler;
+	}
+	
+	public String getName(){
+		return(this.name);
+	}
+	
 }
