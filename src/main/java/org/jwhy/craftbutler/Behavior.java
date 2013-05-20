e1776c18a7053c0b6209ae3df3cbaa5aff7e0d38
Jasper Wiegratz <jwhy@jwhy.de>
2013-05-20 19:26:07 +0200

    Added prefixedCommand and regex mapping methods to behavior template
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- src/main/java/org/jwhy/craftbutler/Behavior.java
+++ src/main/java/org/jwhy/craftbutler/Behavior.java
@@ -1,5 +1,8 @@
 package org.jwhy.craftbutler;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
 public abstract class Behavior {
 
 	public String name;
@@ -9,9 +12,17 @@ public abstract class Behavior {
 	public Behavior(CraftButler butler){
 		this.butler = butler;
 	}
-	
-	public String getName(){
+
+	public final String getName(){
 		return(this.name);
+	}
+
+	public HashMap<String, Method> mapPrefixedCommands() {
+		return(new HashMap<String, Method>());
+	}
+
+	public HashMap<String, Method> mapRegex() {
+		return(new HashMap<String, Method>());
 	}
 	
 }
